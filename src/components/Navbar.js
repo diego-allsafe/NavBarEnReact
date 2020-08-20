19a102eaa4a8e77f80d7ee1e613f7759ecde1846
Diego Goncalvez <diego.data.icloud@gmail.com>
2020-08-20 17:00:08 -0300

    Brevisimo ejemplo

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function NavBar() {
+  return (
+    <nav>
+        <a href="/home">Home</a>
+        <br/>
+        <a href="/productos">Productos</a>
+    </nav>
+  );
+}
+
+export default NavBar;
